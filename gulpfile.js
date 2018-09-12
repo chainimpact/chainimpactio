e95c84cad969fd632ccb089d5809e5655f429539
felipe <felipefaraggi@gmail.com>
2018-09-12 10:12:47 +0200

    automatic image file size reduction

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create(); // create a browser sync instance.
 
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+
+// var imagemin = require('gulp-imagemin');
+// var imageminJpegtran = require('imagemin-jpegtran');
+// var imageminPngquant = require('imagemin-pngquant');
+
 gulp.task('browser-sync', function() {
     bs.init({
         server: {
             baseDir: "./"
         }
     });
+});
+
+gulp.task('imagemin', function() {
+    var imgSrc = 'assets/img/*.+(png|jpg|jpeg|gif)'
+    var imgDist = 'build/images';
+
+    gulp.src(imgSrc)
+    .pipe(changed(imgDist))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDist));
+
+});
+
+gulp.task('default', ['imagemin'],function(){
 });
